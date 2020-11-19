b8bb15f57a79b3f937ae4654021f645f8ca6274f
Chikage <t-9@atx.tokyo>
2020-11-19 18:05:24 +0900

    Get market information

--- docker/go/main.go
+++ docker/go/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	markets, err := getMarkets()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(markets))
+}
+
+func getMarkets() (string, error) {
+	url := "https://api.bitflyer.com/v1/getmarkets"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(byteArray), nil
 }
